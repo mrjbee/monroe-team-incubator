e46d1664e55e1a029626821d25b3d1ddfe38fc52
mrjbee <MisterJBee@gmail.com>
2013-05-05 22:18:05 +0300

    Added clone of example-bundle to test bundle dependencies

---
+++ depended-example-bundle/src/main/java/org/monroe/team/app/arhitect/example/bundle/api/ExampleService.java
@@ -0,0 +1,11 @@
+package org.monroe.team.app.arhitect.example.bundle.api;
+
+/**
+ * User: MisterJBee
+ * Date: 5/5/13 Time: 6:56 PM
+ * Open source: MIT Licence
+ * (Do whatever you want with the source code)
+ */
+public interface ExampleService {
+    void printSomethingWith(String withArg);
+}
