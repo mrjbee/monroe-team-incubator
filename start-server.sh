ca412757a3a51eaaa958941b344690261df21461
mrjbee <MisterJBee@gmail.com>
2013-05-05 21:55:19 +0300

    Startup script

--- start-server.sh
+++ start-server.sh
@@ -2,4 +2,13 @@ cd distribution/target
 rm -rf server
 unzip -u distribution*.zip -d server
 cd server
-java -jar app-arhitect-osgi-core-0.1-SNAPSHOT-jar-with-dependencies.jar -b ./bundles
+touch conf.properties
+echo "felix.auto.deploy.dir=$(pwd)/bundles" >> conf.properties
+echo "felix.auto.deploy.action=install, start" >> conf.properties
+confFile=$(pwd)/conf.properties
+confFileURI=FILE:${confFile}
+echo "==== Using: ${confFile} ===="
+echo "_______________________________________________"
+cat $confFile
+echo "_______________________________________________"
+java -Dfelix.config.properties=$confFileURI -jar app-arhitect-osgi-core-0.1-SNAPSHOT-jar-with-dependencies.jar
