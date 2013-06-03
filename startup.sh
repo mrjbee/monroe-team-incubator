106c488833f9b91ea0cff8e29511f05ec6912768
mrjbee <MisterJBee@gmail.com>
2013-06-03 23:48:28 +0300

    Add startup script

---
+++ startup.sh
@@ -0,0 +1,14 @@
+cd distribution/target
+rm -rf server
+unzip -u distribution*.zip -d server
+cd server
+touch conf.properties
+echo "felix.auto.deploy.dir=$(pwd)/bundles" >> conf.properties
+echo "felix.auto.deploy.action=install, start" >> conf.properties
+confFile=$(pwd)/conf.properties
+confFileURI=FILE:${confFile}
+echo "==== Using: ${confFile} ===="
+echo "_______________________________________________"
+cat $confFile
+echo "_______________________________________________"
+java -Dfelix.config.properties=$confFileURI -jar core-0.1-SNAPSHOT-jar-with-dependencies.jar
