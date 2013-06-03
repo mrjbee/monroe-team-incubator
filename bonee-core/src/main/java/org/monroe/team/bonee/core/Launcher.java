2484b2cf42e2f8d70c85bb4b04cbbe944998ff8f
mrjbee <MisterJBee@gmail.com>
2013-06-03 23:19:21 +0300

    Add core mvn module

---
+++ bonee-core/src/main/java/org/monroe/team/bonee/core/Launcher.java
@@ -0,0 +1,13 @@
+package org.monroe.team.bonee.core;
+
+/**
+ * User: MisterJBee
+ * Date: 6/3/13 Time: 11:12 PM
+ * Open source: MIT Licence
+ * (Do whatever you want with the source code)
+ */
+public class Launcher {
+    public static void main(String[] argv) throws Exception{
+        org.apache.felix.main.Main.main(argv);
+    }
+}
