7fd215cb9ee3b104bc77675593df0048dcdf1441
Dmytro Khudin (mrjbee) <MisterJBee@gmail.com>
2013-06-20 01:11:01 +0300

    Add test project

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
-APP_PATH="application/target"
+cd projects/fserver
+pwd
+APP_PATH="target"
 echo "$APP_PATH"
 LIBS=$(echo $APP_PATH/dependency/*.jar | tr ' ' ':')
 echo "$LIBS"
-java -cp $APP_PATH/application-0.1-SNAPSHOT.jar:$LIBS org.monroe.team.jfeature.application.Main
+java -cp $LIBS org.monroe.team.jfeature.application.Main
