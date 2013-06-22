5740c727edfc8068022ba5e276f498f55c8e3c4f
Dmytro Khudin (mrjbee) <MisterJBee@gmail.com>
2013-06-22 21:20:12 +0300

    Guice integration: Feature life cycle Tested

--- config-feature/src/main/java/org/monroe/team/jfeature/shared/FileSystemConfigFeature.java
+++ config-feature/src/main/java/org/monroe/team/jfeature/shared/FileSystemConfigFeature.java
@@ -1,10 +1,16 @@
 package org.monroe.team.jfeature.shared;
 
+import com.google.inject.TypeLiteral;
 import org.monroe.team.jfeature.Feature;
 import org.monroe.team.jfeature.FeatureInject;
+import org.monroe.team.jfeature.guice.AbstractGuiceFeature;
+import org.monroe.team.jfeature.guice.FeatureStartAware;
 import org.monroe.team.jfeature.shared.api.ApplicationDetailsFeature;
 import org.monroe.team.jfeature.shared.api.ConfigFeature;
 import org.monroe.team.jfeature.shared.api.LoggingFeature;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * User: MisterJBee
@@ -13,7 +19,8 @@ import org.monroe.team.jfeature.shared.api.LoggingFeature;
  * (Do whatever you want with the source code)
  */
 @Feature(impl = ConfigFeature.class)
-public class FileSystemConfigFeature implements ConfigFeature {
+public class FileSystemConfigFeature extends AbstractGuiceFeature<String> implements ConfigFeature {
+
     @FeatureInject
     LoggingFeature loggingFeature;
 
@@ -22,7 +29,26 @@ public class FileSystemConfigFeature implements ConfigFeature {
 
     @Override
     public Class getProperty(String name) {
-        loggingFeature.get("sda").e(new RuntimeException("Test exception:"+applicationDetailsFeature.getAppId()),"");
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        loggingFeature.get("sda").e(new RuntimeException("Test exception:"+applicationDetailsFeature.getAppId()+":"+getImpl()),"");
+        return null;
+    }
+
+    @Override
+    protected Class featureImplClass() {
+        return String.class;
+    }
+
+    @Override
+    protected void configureFeature() {
+        bind(String.class).toInstance("FeatureConfig");
+        List<FeatureStartAware> startAwares = new ArrayList<FeatureStartAware>(1);
+        startAwares.add(new FeatureStartAware() {
+            @Override
+            public void start() {
+                loggingFeature.get("sda").i("Impl:"+getImpl());
+            }
+        });
+        bind(new TypeLiteral<List<FeatureStartAware>>() {
+        }).toInstance(startAwares);
     }
 }
