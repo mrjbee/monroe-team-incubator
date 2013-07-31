1783de60fe69e505af182efce552c1b06ea5a381
Dmytro Khudin (mrjbee) <MisterJBee@gmail.com>
2013-08-01 01:57:06 +0300

    Added support of action bar

--- src/main/java/org/monroe/team/aas/ui/DashboardActivity.java
+++ src/main/java/org/monroe/team/aas/ui/DashboardActivity.java
@@ -1,6 +1,9 @@
 package org.monroe.team.aas.ui;
 
 import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import org.monroe.team.aas.R;
 import roboguice.activity.RoboFragmentActivity;
 
 /**
@@ -10,4 +13,12 @@ import roboguice.activity.RoboFragmentActivity;
  * (Do whatever you want with the source code)
  */
 public class DashboardActivity extends Activity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.dashboard_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 }
