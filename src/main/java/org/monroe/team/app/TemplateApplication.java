62d3ab598ef91cf65e09a9d262ef1feb363b1ce8
Dmytro Khudin (mrjbee) <MisterJBee@gmail.com>
2013-09-21 20:24:43 +0300

    Android minimal setup

---
+++ src/main/java/org/monroe/team/app/TemplateApplication.java
@@ -0,0 +1,12 @@
+package org.monroe.team.app;
+
+import android.app.Application;
+
+/**
+ * User: MisterJBee
+ * Date: 9/21/13 Time: 8:08 PM
+ * Open source: MIT Licence
+ * (Do whatever you want with the source code)
+ */
+public class TemplateApplication extends Application {
+}
