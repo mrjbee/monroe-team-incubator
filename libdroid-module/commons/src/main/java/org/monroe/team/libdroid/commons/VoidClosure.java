cd41ec1893ef69bf743a91440a0eff25d04af29b
Dmytro Khudin (mrjbee) <MisterJBee@gmail.com>
2013-11-22 16:38:36 +0200

    Add VOid closeure

---
+++ libdroid-module/commons/src/main/java/org/monroe/team/libdroid/commons/VoidClosure.java
@@ -0,0 +1,11 @@
+package org.monroe.team.libdroid.commons;
+
+/**
+ * User: MisterJBee
+ * Date: 11/4/13 Time: 11:59 PM
+ * Open source: MIT Licence
+ * (Do whatever you want with the source code)
+ */
+public interface VoidClosure<InType> {
+    public void call(InType in);
+}
