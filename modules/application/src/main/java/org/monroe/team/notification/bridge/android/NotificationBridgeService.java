2e7ee32f52324e31c86adaee555faea581c416c0
Dmytro Khudin (mrjbee) <MisterJBee@gmail.com>
2013-12-13 18:05:20 +0200

    Add model

---
+++ modules/application/src/main/java/org/monroe/team/notification/bridge/android/NotificationBridgeService.java
@@ -0,0 +1,8 @@
+package org.monroe.team.notification.bridge.android;
+
+public class NotificationBridgeService extends org.monroe.team.libdroid.mservice.ModelService<Object>{
+    @Override
+    protected Object createModelInstance() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
