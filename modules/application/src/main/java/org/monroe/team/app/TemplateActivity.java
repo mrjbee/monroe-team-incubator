17ffc3a5994169d5872e590218643614b7831ce0
Dmytro Khudin (mrjbee) <MisterJBee@gmail.com>
2013-12-13 16:07:18 +0200

    Add basic preferneces

--- modules/application/src/main/java/org/monroe/team/app/TemplateActivity.java
+++ modules/application/src/main/java/org/monroe/team/app/TemplateActivity.java
@@ -1,7 +1,9 @@
 package org.monroe.team.app;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
 
 /**
  * User: MisterJBee
@@ -9,9 +11,35 @@ import android.os.Bundle;
  * Open source: MIT Licence
  * (Do whatever you want with the source code)
  */
-public class TemplateActivity extends Activity{
+public class TemplateActivity extends PreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
     }
+
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if ("pref_service_active".equals(key)){
+           boolean serviceBecomeEnabled = (sharedPreferences.getBoolean("pref_service_active",false));
+           getPreferenceScreen().findPreference("pref_out_notif_enable").setEnabled(serviceBecomeEnabled);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
 }
