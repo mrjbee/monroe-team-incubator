a1db0191a2bf67018c7a914a5f1125d0a1580270
mrjbee <MisterJbee@gmail.com>
2014-09-13 19:45:07 +0300

    Chart: add limit draw

--- sources/app/src/main/java/org/monroe/team/smooker/app/SmokeChartView.java
+++ sources/app/src/main/java/org/monroe/team/smooker/app/SmokeChartView.java
@@ -8,13 +8,19 @@ import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.View;
 
-public class ChartView extends View {
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class SmokeChartView extends View {
 
     Paint axisPaint;
     Paint axisLabelPaint;
     Paint transparentPaint;
     Paint backgroundStripePaint;
     Paint stripePaint;
+    Paint limitPaint;
+    Paint limitLabelPaint;
 
     float axisCaptionTextSize =30f;
 
@@ -26,19 +32,21 @@ public class ChartView extends View {
     private float horizontalAxisPadding;
     private float backgroundStripeHeight = 120f;
     private float stripeHeight = 24f;
+    private List<Date> model = new ArrayList<Date>();
 
+    private int limit = -1;
 
-    public ChartView(Context context) {
+    public SmokeChartView(Context context) {
         super(context);
         initialize(context);
     }
 
-    public ChartView(Context context, AttributeSet attrs) {
+    public SmokeChartView(Context context, AttributeSet attrs) {
         super(context, attrs);
         initialize(context);
     }
 
-    public ChartView(Context context, AttributeSet attrs, int defStyleAttr) {
+    public SmokeChartView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         initialize(context);
     }
@@ -66,9 +74,21 @@ public class ChartView extends View {
         backgroundStripePaint.setAlpha(5);
 
         stripePaint = new Paint();
-        stripePaint.setAntiAlias(true);
         stripePaint.setColor(Color.BLACK);
         stripePaint.setAlpha(30);
+
+        limitPaint = new Paint();
+        limitPaint.setColor(Color.RED);
+        limitPaint.setStrokeWidth(4);
+
+        limitLabelPaint = new Paint(Paint.ANTI_ALIAS_FLAG | Paint.LINEAR_TEXT_FLAG);
+        limitLabelPaint.setColor(Color.RED);
+        limitLabelPaint.setTextSize(axisCaptionTextSize);
+
+
+        //DEBUG
+        limit = 15;
+        model = new ArrayList<Date>(10);
     }
 
     @Override
@@ -76,11 +96,33 @@ public class ChartView extends View {
         drawBackgroundStripes(canvas);
         float topPadding = drawStripes(canvas);
         drawAxis(canvas, topPadding);
+        if (limit > 0){
+            drawLimit(canvas);
+        }
+    }
+
+    private void drawLimit(Canvas canvas) {
+        float itemHeight = getItemHeight();
+        float limitYPosition = getHeight()-horizontalAxisPadding-itemHeight*limit;
+
+        Rect textBounds = new Rect();
+        String limitAsText = Integer.toString(limit);
+        limitLabelPaint.getTextBounds(limitAsText, 0, limitAsText.length(),textBounds);
+
+        canvas.drawLine(verticalAxisPadding + textBounds.width() + verticalAxisTextBounds.height() * 1.5f,
+                limitYPosition,
+                getWidth(),
+                limitYPosition,
+                limitPaint);
+
+        canvas.drawText(limitAsText, verticalAxisPadding + verticalAxisTextBounds.height(), limitYPosition  + textBounds.width() * 0.25f, limitLabelPaint);
+
+
     }
 
     private float drawStripes(Canvas canvas) {
 
-        int stripeCount = (int) ((getHeight() - horizontalAxisPadding) / stripeHeight);
+        int stripeCount = maxItemsOnABoard();
 
         for (int i = 0; i < stripeCount; i++){
             canvas.drawLine(
@@ -93,6 +135,10 @@ public class ChartView extends View {
 
         }
         return getHeight() - (stripeCount-1)*stripeHeight - horizontalAxisPadding;
+    }
+
+    private int maxItemsOnABoard() {
+        return (int) ((getHeight() - horizontalAxisPadding) / stripeHeight);
     }
 
     private void drawBackgroundStripes(Canvas canvas) {
@@ -114,7 +160,6 @@ public class ChartView extends View {
 
 
     private void drawAxis(Canvas canvas, float topPadding) {
-
         //DEBUG
         //canvas.drawText(verticalAxisTextBounds.width()+":"+verticalAxisTextBounds.height(),200,200,axisPaint);
 
@@ -143,5 +188,16 @@ public class ChartView extends View {
                 getWidth() - horizontalAxisTextBounds.width() * 1.5f,
                 getHeight() - horizontalAxisPadding + horizontalAxisTextBounds.height() * 0.25f,
                 axisLabelPaint);
+    }
+
+    private float getItemHeight() {
+       float max = maxItemsOnABoard();
+       float maxToDraw = Math.max(limit,model.size());
+       if (max >= maxToDraw){
+           //1 stripe : 1 smoke
+           return stripeHeight;
+       } else {
+           return stripeHeight / ((maxToDraw+maxToDraw/5) / max);
+       }
     }
 }
