e305b04c61f2f265f9c996ab8f46ac73fabfe59c
mrjbee <MisterJbee@gmail.com>
2014-09-13 18:56:29 +0300

    Chart: added field

--- sources/app/src/main/java/org/monroe/team/smooker/app/ChartView.java
+++ sources/app/src/main/java/org/monroe/team/smooker/app/ChartView.java
@@ -13,18 +13,19 @@ public class ChartView extends View {
     Paint axisPaint;
     Paint axisLabelPaint;
     Paint transparentPaint;
+    Paint backgroundStripePaint;
     Paint stripePaint;
 
-    float axisPadding = 70f;
-    float stripeHeight = 70f;
     float axisCaptionTextSize =30f;
 
     String verticalAxisName = "smokes count";
     String horizontalAxisName = "today hours";
-    Rect axisVerticalTextBounds = new Rect();
+    Rect verticalAxisTextBounds = new Rect();
     private float verticalAxisPadding;
-    Rect axisHorizontalTextBounds = new Rect();
-    private float horisontalAxisPadding;
+    Rect horizontalAxisTextBounds = new Rect();
+    private float horizontalAxisPadding;
+    private float backgroundStripeHeight = 120f;
+    private float stripeHeight = 24f;
 
 
     public ChartView(Context context) {
@@ -51,53 +52,96 @@ public class ChartView extends View {
         axisLabelPaint.setColor(Color.BLACK);
         axisLabelPaint.setTextSize(axisCaptionTextSize);
 
-        axisLabelPaint.getTextBounds(verticalAxisName,0,verticalAxisName.length(), axisVerticalTextBounds);
-        verticalAxisPadding = axisVerticalTextBounds.height()/2;
+        axisLabelPaint.getTextBounds(verticalAxisName,0,verticalAxisName.length(), verticalAxisTextBounds);
+        verticalAxisPadding = verticalAxisTextBounds.height()/2;
 
-        axisLabelPaint.getTextBounds(horizontalAxisName,0,horizontalAxisName.length(),axisHorizontalTextBounds);
-        horisontalAxisPadding = axisHorizontalTextBounds.height() * 2.5f;
+        axisLabelPaint.getTextBounds(horizontalAxisName,0,horizontalAxisName.length(), horizontalAxisTextBounds);
+        horizontalAxisPadding = horizontalAxisTextBounds.height() * 2.5f;
 
         transparentPaint = new Paint();
         transparentPaint.setColor(Color.TRANSPARENT);
 
+        backgroundStripePaint = new Paint();
+        backgroundStripePaint.setColor(Color.BLACK);
+        backgroundStripePaint.setAlpha(5);
+
         stripePaint = new Paint();
-        stripePaint.setColor(Color.RED);
-        stripePaint.setAlpha(50);
+        stripePaint.setAntiAlias(true);
+        stripePaint.setColor(Color.BLACK);
+        stripePaint.setAlpha(30);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        drawAxis(canvas);
+        drawBackgroundStripes(canvas);
+        float topPadding = drawStripes(canvas);
+        drawAxis(canvas, topPadding);
+    }
+
+    private float drawStripes(Canvas canvas) {
+
+        int stripeCount = (int) ((getHeight() - horizontalAxisPadding) / stripeHeight);
+
+        for (int i = 0; i < stripeCount; i++){
+            canvas.drawLine(
+                        verticalAxisPadding,
+                        getHeight() - horizontalAxisPadding - (stripeHeight * i),
+                        getWidth(),
+                        getHeight() - horizontalAxisPadding - (stripeHeight * i),
+                        stripePaint
+                );
+
+        }
+        return getHeight() - (stripeCount-1)*stripeHeight - horizontalAxisPadding;
+    }
+
+    private void drawBackgroundStripes(Canvas canvas) {
+
+        int backgroundStripeCount = (int) ((getHeight() - horizontalAxisPadding) / backgroundStripeHeight);
+
+        for (int i = 0; i < backgroundStripeCount; i++){
+            if (i % 2 == 0) {
+                canvas.drawRect(
+                        verticalAxisPadding,
+                        getHeight() - horizontalAxisPadding - (backgroundStripeHeight * (i + 1)),
+                        getWidth(),
+                        getHeight() - horizontalAxisPadding - (backgroundStripeHeight * i),
+                        backgroundStripePaint
+                );
+            }
+        }
     }
 
 
-    private void drawAxis(Canvas canvas) {
+    private void drawAxis(Canvas canvas, float topPadding) {
 
         //DEBUG
-        //canvas.drawText(axisVerticalTextBounds.width()+":"+axisVerticalTextBounds.height(),200,200,axisPaint);
+        //canvas.drawText(verticalAxisTextBounds.width()+":"+verticalAxisTextBounds.height(),200,200,axisPaint);
 
-        canvas.drawLine(verticalAxisPadding, 0, verticalAxisPadding, axisVerticalTextBounds.width() * 0.5f, axisPaint);
-        canvas.drawLine(verticalAxisPadding, axisVerticalTextBounds.width() * 1.5f, verticalAxisPadding, getHeight() - horisontalAxisPadding, axisPaint);
+        if (topPadding < verticalAxisTextBounds.width() * 0.5f){
+            canvas.drawLine(verticalAxisPadding, topPadding, verticalAxisPadding, verticalAxisTextBounds.width() * 0.5f, axisPaint);
+        }
+        canvas.drawLine(verticalAxisPadding, verticalAxisTextBounds.width() * 1.5f, verticalAxisPadding, getHeight() - horizontalAxisPadding, axisPaint);
 
         canvas.rotate(-90);
-        canvas.drawText(verticalAxisName, -axisVerticalTextBounds.width() * 1.5f, axisVerticalTextBounds.height()*0.75f, axisLabelPaint);
+        canvas.drawText(verticalAxisName, -verticalAxisTextBounds.width() * 1.5f, verticalAxisTextBounds.height()*0.75f, axisLabelPaint);
         canvas.rotate(90);
 
         canvas.drawLine(verticalAxisPadding,
-                        getHeight() - horisontalAxisPadding,
-                        getWidth() - axisHorizontalTextBounds.width()*1.5f,
-                        getHeight() - horisontalAxisPadding,
+                        getHeight() - horizontalAxisPadding,
+                        getWidth() - horizontalAxisTextBounds.width()*1.5f,
+                        getHeight() - horizontalAxisPadding,
                 axisPaint);
 
         canvas.drawLine(getWidth(),
-                getHeight() - horisontalAxisPadding,
-                getWidth() - axisHorizontalTextBounds.width()*.5f,
-                getHeight() - horisontalAxisPadding,
+                getHeight() - horizontalAxisPadding,
+                getWidth() - horizontalAxisTextBounds.width()*.5f,
+                getHeight() - horizontalAxisPadding,
                 axisPaint);
 
         canvas.drawText(horizontalAxisName,
-                getWidth() - axisHorizontalTextBounds.width()*1.5f,
-                getHeight() - horisontalAxisPadding + axisHorizontalTextBounds.height()*0.25f,
+                getWidth() - horizontalAxisTextBounds.width() * 1.5f,
+                getHeight() - horizontalAxisPadding + horizontalAxisTextBounds.height() * 0.25f,
                 axisLabelPaint);
     }
 }
