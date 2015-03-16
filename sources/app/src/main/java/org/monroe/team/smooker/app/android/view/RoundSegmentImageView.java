b31a0e2f275c8791cfdba9b211d46102d7f1cd24
Dmytro (Mrjbee) Khudin <MisterJbee@gmail.com>
2015-03-16 19:19:35 +0200

    Clock: optimize clock view

--- sources/app/src/main/java/org/monroe/team/smooker/app/android/view/RoundSegmentImageView.java
+++ sources/app/src/main/java/org/monroe/team/smooker/app/android/view/RoundSegmentImageView.java
@@ -16,24 +16,42 @@ import android.widget.ImageView;
 public class RoundSegmentImageView extends ImageView{
 
     private float angle;
+    private Bitmap originImageBitmap;
+    private Bitmap finalImageBitmap;
+    private RectF lastUsedArchRect;
+
+    private Paint maskDefinePaint;
+    private Paint maskApplyPaint;
 
     public RoundSegmentImageView(Context context) {
         super(context);
+        init();
     }
 
     public RoundSegmentImageView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public RoundSegmentImageView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
     }
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public RoundSegmentImageView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+        init();
     }
 
+    private void init() {
+        maskDefinePaint = new Paint();
+        maskDefinePaint.setColor(Color.BLACK);
+
+        maskApplyPaint = new Paint();
+        maskApplyPaint.setAntiAlias(true);
+        maskApplyPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+    }
 
     public float getAngle() {
         return angle;
@@ -46,33 +64,32 @@ public class RoundSegmentImageView extends ImageView{
     //TODO: require optimization
     @Override
     protected void onDraw(Canvas canvas) {
-
-        Paint paint = new Paint();
-        paint.setColor(Color.BLACK);
-
-        RectF archRect = new RectF(0,0,getWidth(),getHeight());
         float startAngle = -90;
+        if (lastUsedArchRect == null ||
+                lastUsedArchRect.width() != getWidth() ||
+                lastUsedArchRect.height() != getHeight()){
+            lastUsedArchRect = new RectF(0,0,getWidth(),getHeight());
+            finalImageBitmap = Bitmap.createBitmap(getWidth(), getHeight(), Bitmap.Config.ARGB_8888);
+            originImageBitmap = Bitmap.createBitmap(getWidth(), getHeight(), Bitmap.Config.ARGB_8888);
+            super.onDraw(new Canvas(originImageBitmap));
+        }
 
-        Bitmap originImageBitmap = Bitmap.createBitmap(getWidth(), getHeight(), Bitmap.Config.ARGB_8888);
-        super.onDraw(new Canvas(originImageBitmap));
+        finalImageBitmap.eraseColor(Color.TRANSPARENT);
+        Canvas finalCanvas = new Canvas(finalImageBitmap);
+        finalCanvas.drawArc(lastUsedArchRect, startAngle, angle, true, maskDefinePaint);
+        finalCanvas.drawBitmap(originImageBitmap, 0, 0, maskApplyPaint);
 
-        Bitmap shadowImageBitmap = Bitmap.createBitmap(getWidth(), getHeight(), Bitmap.Config.ARGB_8888);
-        Canvas shadowImageCanvas = new Canvas(shadowImageBitmap);
+        debug(canvas, startAngle);
 
-        shadowImageCanvas.drawArc(archRect, startAngle, angle, true, paint);
+        canvas.drawBitmap(finalImageBitmap, 0, 0, null);
+    }
 
-        final Paint shadowPaint = new Paint();
-        shadowPaint.setAntiAlias(true);
-        shadowPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
-        shadowImageCanvas.drawBitmap(originImageBitmap, 0, 0, shadowPaint);
-
+    private void debug(Canvas canvas, float startAngle) {
         if (false) {
             //DEBUG
             Paint debug = new Paint();
             debug.setColor(Color.RED);
-            canvas.drawArc(archRect, startAngle, angle, true, debug);
+            canvas.drawArc(lastUsedArchRect, startAngle, angle, true, debug);
         }
-        canvas.drawBitmap(shadowImageBitmap, 0, 0, null);
-
     }
 }
